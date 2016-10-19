fe66afbd34ac5f69fa5a2790f58c1e9e489453ae
Jeff Swearingen <jeffswearingen@gmail.com>
2016-10-19 09:54:59 -0400

    updated game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,16 @@ var hiddenWord = [];
 var tempWord = "";
 var reset = true;
 var wins = 0;
+var losses = 0;
 var guesses = 5;
 var guessed = [];
+var currentWordConversion = [];
+var hiddenWordString = "";
+
 function pickAWord() {
 	reset = false;
 	hiddenWord = [];
-	currentWord = 'great intelligence';
-//	currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+	currentWord = wordList[Math.floor(Math.random() * wordList.length)];
 	for (var i = 0; i < currentWord.length; i++) {
 		if (currentWord.charAt(i) != ' ') {
 			hiddenWord[i] = '-';
@@ -20,11 +23,21 @@ function pickAWord() {
 			hiddenWord[i] = '_';
 		}
 	}
-	document.getElementById('currentWord').innerHTML = currentWord;
+	for (var k = 0; k < currentWord.length; k++) {
+		currentWordConversion[k] = currentWord.charAt(k);
+		if (currentWord.charAt(k) == ' ') { 
+			currentWordConversion[k] = '_';
+		}
+	}
+	console.log(currentWord);
+	console.log(currentWordConversion);
+//	document.getElementById('currentWord').innerHTML = currentWord;
 	document.getElementById('hiddenWord').innerHTML = hiddenWord.join(' ');
-	console.log(currentWord + " " + hiddenWord + " " + reset);
 	guessed = [];
+	document.getElementById('guessed').innerHTML = guessed.join(' ');
 	guesses = 5;
+	document.getElementById('guesses').innerHTML = guesses;
+
 }
 
 function setup(reset) {
@@ -37,14 +50,8 @@ function setup(reset) {
 }
 
 function checkForWin() {
-	var currentWordConversion = [];
-	for (var k = 0; k < currentWord.length; k++) {
-		currentWordConversion[k] = currentWord.charAt(k);
-		if (currentWord.charAt(k) == ' ') { 
-			currentWordConversion[k] = '_';
-		}
-	}
-	if (hiddenWord === currentWordConversion) {
+
+	if (hiddenWord == currentWordConversion) {
 		wins += 1;
 		document.getElementById('wins').innerHTML = wins;
 		pickAWord();
@@ -58,26 +65,20 @@ document.onkeyup = function(event) {
 	eventInput = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("eventInput " + eventInput);
 	setup(reset);
+	// if eventInput is in currentWord, change the hiddenWord array element to match event Input
 	for (var j = 0; j < currentWord.length; j++) {
 		if (eventInput == currentWord.charAt(j)) {
-//			if (eventInput = ' ') { 
-//				hiddenWord[j] = '_';
-//			} else {
-				hiddenWord[j] = eventInput;
-			
-//			}
-//			tempWord += eventInput;
-		}// else if (hiddenWord.charAt(j) != "_") {
-//			tempWord += hiddenWord.charAt(j);
-//		} else {
-//			tempWord += "_";
-//		}
-
+				hiddenWord[j] = eventInput;			
+		}
 	}
+	// create hidden word string from hidden word for comparison
+	hiddenWordString = "";
+	for (var c = 0; c < hiddenWord.length; c++) {
+		hiddenWordString += hiddenWord[c];
+	}
+	// add elements to guessed array.  if eventInput not found in currentword, decrease guesses.
+	//display guesses and guessed updates
 	if (eventInput != " ") {
-		console.log("test");
-		console.log(eventInput);
-		console.log(guessed);
 		if (guessed.indexOf(eventInput) == -1) {
 			guessed.push(eventInput);
 			if (currentWord.indexOf(eventInput) == -1) {
@@ -88,12 +89,27 @@ document.onkeyup = function(event) {
 		}
 
 	}
+	// if guesses is zero, increase losses
+	// display losses
+	// pick a new word
 	if (guesses <= 0) {
-		document.getElementById('alert').innerHTML = "Game Over";
+		losses++;
+		document.getElementById('losses').innerHTML = losses;
 		pickAWord();
 	}
-//	hiddenWord = tempWord;
-//	tempWord = "";
+
+	// debugging
+	console.log(hiddenWordString + " " + currentWord);
+
+
+	// check for a win
+		if (hiddenWordString == currentWord) {
+		wins += 1;
+		document.getElementById('wins').innerHTML = wins;
+		pickAWord();
+
+	}
+	// update hidden word for display
 	document.getElementById('hiddenWord').innerHTML = hiddenWord.join(' ');
-	checkForWin();
+
 }	
